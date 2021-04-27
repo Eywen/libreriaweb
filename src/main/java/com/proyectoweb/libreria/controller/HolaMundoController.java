f486901f686f8d67cc4f55d62b152da27a93343e
Kara <blk20100@gmail.com>
2021-04-27 22:26:37 +0200

    primer commit

---
+++ src/main/java/com/proyectoweb/libreria/controller/HolaMundoController.java
@@ -0,0 +1,14 @@
+package com.proyectoweb.libreria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+
+    @RequestMapping("/")
+    public String holaMundo() {
+
+        return "Hola Mundo";
+    }
+}
